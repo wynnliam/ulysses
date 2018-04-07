13cc5aeaac9a7ad7586b75ee05cdac4215242ed4
Liam Wynn <wynnliam@pdx.edu>
2018-04-07 11:12:23 -0700

    Added class AtmosphereGenerator

---
+++ src/ulysses/planet/utilities/generators/AtmosphereGenerator.java
@@ -0,0 +1,19 @@
+// Liam Wynn, 4/7/2018, Ulysses
+
+/*
+	Manages the logic of generating an Atmosphere.
+
+	TODO: Finish me!
+*/
+
+package ulysses.planet.utilities.generators;
+
+public class AtmosphereGenerator {
+	// Dimensions of the atmosphere we wish to generate.
+	private int width, height;
+
+	public AtmosphereGenerator() {
+		this.width = 256;
+		this.height = 128;
+	}
+}
