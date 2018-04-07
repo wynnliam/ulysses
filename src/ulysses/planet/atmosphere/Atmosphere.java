e28bf9da57a18fb8066a1028bcbd03c9c724c1c8
Liam Wynn <wynnliam@pdx.edu>
2018-04-07 10:18:23 -0700

    Added class Atmosphere

---
+++ src/ulysses/planet/atmosphere/Atmosphere.java
@@ -0,0 +1,21 @@
+// Liam Wynn, 4/7/2018, Ulysses
+
+/*
+	The Atmosphere is responsible for a Planet's
+	temperature data. The temperature data is represented
+	in the form of the Temperature Map - which is the
+	temperatures of every land point. The Temperature map
+	is a combination of several submaps as follows:
+
+	Altitude - The height of each point.
+	Latitude - The distance each point is from a specified
+	equator.
+	Wind - Essentially a smooth noise map that represents wind
+	patterns and their effects on the temperature.
+	Distance to sea - TODO: Finish me!
+*/
+
+package ulysses.planet;
+
+public class Atmosphere {
+}
