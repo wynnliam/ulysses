422e931192a9d21cac697884e136bc2876730d94
Liam Wynn <wynnliam@pdx.edu>
2018-04-15 19:04:45 -0700

    Added accessors and mutators for class BiosphereGenerator

--- src/ulysses/planet/utilities/generators/BiosphereGenerator.java
+++ src/ulysses/planet/utilities/generators/BiosphereGenerator.java
@@ -23,4 +23,50 @@ public class BiosphereGenerator {
 		this.tempMap = null;
 		this.precipMap = null;
 	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public void setWidth(int val) {
+		if(val <= 0)
+			val = 256;
+
+		this.width = val;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public void setHeight(int val) {
+		if(val <= 0)
+			val = 128;
+
+		this.height = val;
+	}
+
+	public PlanetMap getHeightMap() {
+		return this.heightMap;
+	}
+
+	public void setHeightMap(PlanetMap val) {
+		this.heightMap = val;
+	}
+
+	public PlanetMap getTemperatureMap() {
+		return this.tempMap;
+	}
+
+	public void setTemperatureMap(PlanetMap val) {
+		this.tempMap = val;
+	}
+
+	public PlanetMap getPrecipitationMap() {
+		return this.precipMap;
+	}
+
+	public void setPrecipitationMap(PlanetMap val) {
+		this.precipMap = val;
+	}
 }
